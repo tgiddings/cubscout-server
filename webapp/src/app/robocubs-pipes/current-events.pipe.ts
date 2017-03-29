fa0990beaf2802d15ba424d2624957ca80e37f7d
trevor <tgiddings000@gmail.com>
2017-03-28 17:13:14 -0700

    remove spammy debug log statement
    Signed-off-by: trevor <tgiddings000@gmail.com>

--- webapp/src/app/robocubs-pipes/current-events.pipe.ts
+++ webapp/src/app/robocubs-pipes/current-events.pipe.ts
@@ -3,7 +3,6 @@ import {Event} from "../event";
 @Pipe({name:"currentEvents",pure:false})
 export class CurrentEventsPipe implements PipeTransform{
   transform(value: any, ...args: any[]): any {
-    console.log(value);
     return (<Event[]>value).filter(event=>(event.startDate==null||event.startDate<=new Date())&&(event.endDate==null||event.endDate>=new Date()));
   }
 
